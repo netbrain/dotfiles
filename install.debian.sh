415e35b69d3f5f0c9700c6ce295904db8e8862a1
Kim Eik <kim@heldig.org>
2018-10-10 11:31:58 +0200

    updated configuration

--- install.debian.sh
+++ install.debian.sh
@@ -1,53 +1,13 @@
 #!/bin/bash -xe
-sudo apt-get update
 
-#BASE INSTALL
-read -r -d '' PACKAGES << EOM #Packages to install
-git
-aria2c
-make
-EOM
-echo $PACKAGES | xargs sudo -E apt-get install -y
-
-#APT-FAST
-git clone https://github.com/ilikenwf/aptfast /tmp/aptfast
-cd /tmp/aptfast
-make install
-
-#FAST BASE INSTALL
-read -r -d '' PACKAGES << EOM #Packages to install
-acpi
-acpid
-build-essential
-curl
-fonts-dejavu
-fonts-dejavu-core
-fonts-dejavu-extra
-fonts-inconsolata
-fonts-vlgothic
-i3
-strace
-sudo
-ttf-dejavu
-ttf-dejavu-core
-ttf-dejavu-extra
-vim-nox
-xorg
-xclip
-feh
-compton
-tmux
-xbindkeys
-xterm
-wicd-gtk
-EOM
-echo $PACKAGES | xargs sudo -E apt-fast install -y
+#BASE SYSTEM
+su -l -c "cd $PWD && apt-get update && cat install.debian.packages | xargs apt-get install -y"
 
 #DOCKER INSTALL
-curl -sSL https://get.docker.com/ | sudo sh
-sudo adduser $USER docker
+#curl -sSL https://get.docker.com/ | sudo sh
+#sudo adduser $USER docker
 
-DEVPATH=$HOME/dev
-BINPATH=$HOME/bin
-TOOLSPATH=$HOME/tools
-mkdir -p $DEVPATH $BINPATH $TOOLSPATH
+#DEVPATH=$HOME/dev
+#BINPATH=$HOME/bin
+#TOOLSPATH=$HOME/tools
+#mkdir -p $DEVPATH $BINPATH $TOOLSPATH
